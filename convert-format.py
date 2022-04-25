d83bee62e9e4504f63bd08d6ed7b25a1b141c10b
Afroz Alam <afrozalm@berkeley.edu>
2022-04-24 18:03:27 -0700

    add weights by normal distributions

--- convert-format.py
+++ convert-format.py
@@ -1,10 +1,14 @@
 ### Opening files
+import numpy as np
 
-filename_in = './wikiElec.ElecBs3.txt'
+np.random.seed(1729)
+
+filename_in = './wikiElec.txt'
 fi = open(filename_in, 'r', errors='replace')
 lines = fi.readlines()[6:]
 
 filename_out = f'{filename_in[:-3]}triples'
+print(f'filename_out is {filename_out}')
 fo = open(filename_out, 'w')
 fo.write('#!c++\n')
 
@@ -15,9 +19,12 @@ current_nominee = None
 
 def write_vote(nominee, voter, vote):
     # If they are neutral, we don't write this vote
-    if (vote != '0'):
+    if vote in ['-1', '0', '1']:
         if (nominee) and (voter):
-            fo.write(f'{voter}\t{nominee}\t{vote}\n')
+            _vote = int(vote)
+            mean = 0.5 + _vote * 0.4
+            score = max(1e-5, np.random.normal(mean, 0.001))
+            fo.write(f'{voter}\t{nominee}\t{score}\n')
         else:
             print(Exception(f'undefined nodes!: n={nominee} v={voter}'))
     
@@ -29,4 +36,6 @@ for line in lines:
         vote = line.split('\t')[1]
         voter = line.split('\t')[-1][:-1]
         write_vote(current_nominee, voter, vote)
-    
+
+fo.close()
+
